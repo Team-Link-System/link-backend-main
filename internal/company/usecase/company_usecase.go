62b2f63461e31777190eb4e20ec571d33f2d83c7
이현종 <ehdclr@gractor.com>
2024-10-23 17:58:41 +0900

    할 일 주석 추가

--- internal/company/usecase/company_usecase.go
+++ internal/company/usecase/company_usecase.go
@@ -14,3 +14,9 @@ type companyUsecase struct {
 func NewCompanyUsecase(companyRepository repository.CompanyRepository) CompanyUsecase {
 	return &companyUsecase{companyRepository: companyRepository}
 }
+
+//TODO 회사 조회
+
+//TODO 회사 삭제 (회사 관리자만 - 자기 회사 삭제 가능) -> 구독을 끊는건지 회사 삭제인지 확인 필요
+
+//TODO 회사 구독 취소 (회사 관리자만 - 자기 회사 구독 취소 가능)
