1fa77256898ec324c558f6081875dc7e69ca6a04
이현종 <ehdclr@gractor.com>
2024-10-31 12:44:24 +0900

    서버 재시작 panic recover

--- pkg/interceptor/error_handler.go
+++ pkg/interceptor/error_handler.go
@@ -4,8 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"link/pkg/common"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,16 +11,20 @@ import (
 func ErrorHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next() // 요청 처리 진행
+
 		err := c.Errors.Last()
 		if err != nil {
-			statusCode, ok := err.Meta.(int)
-			if !ok {
-				statusCode = http.StatusInternalServerError
+			statusCode := http.StatusInternalServerError
+			if meta, ok := err.Meta.(int); ok {
+				statusCode = meta
 			}
 
-			// 에러 응답을 Response 구조체로 처리하여 JSON으로 반환
-			log.Printf("에러 발생: %v", err.Error())
-			c.JSON(statusCode, common.NewError(statusCode, err.Error()))
+			log.Printf(
+				"[에러] 경로: %s, 메소드: %s, 클라이언트 IP: %s, 상태 코드: %d, 에러 메시지: %s",
+				c.FullPath(), c.Request.Method, c.ClientIP(), statusCode, err.Error(),
+			)
+
+			c.JSON(statusCode, gin.H{"error": err.Error()})
 		}
 	}
 }
