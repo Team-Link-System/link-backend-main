5cf426a16413a3cb989962589d1a5a17dd937006
이현종 <ehdclr@gractor.com>
2024-11-25 17:02:47 +0900

    comment 응답 DTO 작성

---
+++ pkg/dto/res/comment_res.go
@@ -0,0 +1,41 @@
+package res
+
+type CommentResponse struct {
+	CommentId   uint   `json:"comment_id"`
+	UserId      uint   `json:"user_id"`
+	Content     string `json:"content"`
+	IsAnonymous bool   `json:"is_anonymous"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
+type ReplyResponse struct {
+	CommentId   uint   `json:"comment_id"`
+	UserId      uint   `json:"user_id"`
+	ParentID    uint   `json:"parent_id"`
+	Content     string `json:"content"`
+	IsAnonymous bool   `json:"is_anonymous"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
+// TODO parentId 없는 댓글은 무한스크롤 (커서)로 처리
+type CommentMeta struct {
+	NextCursor string `json:"next_cursor,omitempty"`
+	HasMore    *bool  `json:"has_more,omitempty"`
+	TotalCount int    `json:"total_count"`
+	TotalPages int    `json:"total_pages,omitempty"`
+	PageSize   int    `json:"page_size"`
+	PrevPage   int    `json:"prev_page,omitempty"`
+	NextPage   int    `json:"next_page,omitempty"`
+}
+
+type GetCommentsResponse struct {
+	Comments []*CommentResponse `json:"comments"`
+	Meta     *CommentMeta       `json:"meta"`
+}
+
+type GetRepliesResponse struct {
+	Replies []*ReplyResponse `json:"replies"`
+	Meta    *CommentMeta     `json:"meta"`
+}
