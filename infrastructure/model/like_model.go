23a20f460480e6f5ef7c81764296c1321d940f92
이현종 <ehdclr@gractor.com>
2024-11-27 16:00:12 +0900

    좋아요 모델 변경 이모지 모델 변경 다다형적 관계 추가

--- infrastructure/model/like_model.go
+++ infrastructure/model/like_model.go
@@ -1,15 +1,11 @@
 package model
 
 type Like struct {
-	ID        uint     `gorm:"primaryKey"`
-	UserID    uint     `gorm:"not null"`
-	User      *User    `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`                     // 사용자와의 관계
-	PostID    *uint    `gorm:""`                                                                  // 게시물과의 관계 (null 허용)
-	Post      *Post    `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE;OnUpdate:CASCADE"`    // 게시물이 삭제되면 연관된 Like도 삭제
-	CommentID *uint    `gorm:""`                                                                  // 댓글과의 관계 (null 허용)
-	Comment   *Comment `gorm:"foreignKey:CommentID;constraint:OnDelete:CASCADE;OnUpdate:CASCADE"` // 댓글이 삭제되면 연관된 Like도 삭제
-	EmojiID   uint     `gorm:"not null"`
-	Emoji     *Imogi   `gorm:"foreignKey:EmojiID;constraint:OnDelete:CASCADE;OnUpdate:CASCADE"` // 이모지와의 관계
-	//TODO 이모지 테이블 따로 생성 - 이모지 추가 시 추가 가능하게
-	//TODO 이모지 테이블에 추가된 이모지 추가 시 추가 가능하게 (관리자만)
+	ID         uint   `gorm:"primaryKey"`
+	UserID     uint   `gorm:"not null"`
+	User       *User  `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"` // 사용자와의 관계
+	TargetType string `gorm:"not null"`
+	TargetID   uint   `gorm:"not null"`
+	EmojiID    uint   `gorm:"not null"`
+	Emoji      *Imogi `gorm:"foreignKey:EmojiID;constraint:OnDelete:CASCADE;OnUpdate:CASCADE"` // 이모지와의 관계
 }
