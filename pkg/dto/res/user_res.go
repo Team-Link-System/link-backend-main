67012006e5c2b7b345d75277c3dfabb6f2b639ac
이현종 <ehdclr@gractor.com>
2025-01-07 13:01:16 +0900

    마이페이지 res 수정

--- pkg/dto/res/user_res.go
+++ pkg/dto/res/user_res.go
@@ -16,27 +16,28 @@ type GetUsersByCompanyResponse struct {
 }
 
 type GetUserByIdResponse struct {
-	ID              uint       `json:"id"`
-	Email           string     `json:"email"`
-	Name            string     `json:"name,omitempty"`
-	Phone           string     `json:"phone,omitempty"`
-	Nickname        string     `json:"nickname,omitempty"`
-	IsOnline        bool       `json:"is_online"`
-	IsSubscribed    bool       `json:"is_subscribed"`
-	Role            uint       `json:"role,omitempty"`
-	Image           string     `json:"image,omitempty"`
-	Birthday        string     `json:"birthday,omitempty"`
-	CompanyID       uint       `json:"company_id,omitempty"`
-	CompanyName     string     `json:"company_name,omitempty"`
-	DepartmentIds   []uint     `json:"department_ids,omitempty"`
-	DepartmentNames []string   `json:"department_names,omitempty"`
-	TeamIds         []uint     `json:"team_ids,omitempty"`
-	TeamNames       []string   `json:"team_names,omitempty"`
-	PositionId      uint       `json:"position_id,omitempty"`
-	PositionName    string     `json:"position_name,omitempty"`
-	EntryDate       *time.Time `json:"entry_date,omitempty"`
-	CreatedAt       time.Time  `json:"created_at,omitempty"`
-	UpdatedAt       time.Time  `json:"updated_at,omitempty"`
+	ID              uint                     `json:"id"`
+	Email           string                   `json:"email"`
+	Name            string                   `json:"name,omitempty"`
+	Phone           string                   `json:"phone,omitempty"`
+	Nickname        string                   `json:"nickname,omitempty"`
+	IsOnline        bool                     `json:"is_online"`
+	IsSubscribed    bool                     `json:"is_subscribed"`
+	Role            uint                     `json:"role,omitempty"`
+	Image           string                   `json:"image,omitempty"`
+	Birthday        string                   `json:"birthday,omitempty"`
+	CompanyID       uint                     `json:"company_id,omitempty"`
+	CompanyName     string                   `json:"company_name,omitempty"`
+	DepartmentIds   []uint                   `json:"department_ids,omitempty"`
+	DepartmentNames []string                 `json:"department_names,omitempty"`
+	Departments     []map[string]interface{} `json:"departments,omitempty"`
+	TeamIds         []uint                   `json:"team_ids,omitempty"`
+	TeamNames       []string                 `json:"team_names,omitempty"`
+	PositionId      uint                     `json:"position_id,omitempty"`
+	PositionName    string                   `json:"position_name,omitempty"`
+	EntryDate       *time.Time               `json:"entry_date,omitempty"`
+	CreatedAt       time.Time                `json:"created_at,omitempty"`
+	UpdatedAt       time.Time                `json:"updated_at,omitempty"`
 }
 
 type SearchUserResponse struct {
