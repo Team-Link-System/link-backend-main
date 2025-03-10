e06509c60042bc7f5d63323bce39ee581dad349d
이현종 <ehdclr@gractor.com>
2025-03-10 11:55:01 +0900

    feat: add project role validation for update functionality

--- pkg/http/project_handler.go
+++ pkg/http/project_handler.go
@@ -205,34 +205,40 @@ func (h *ProjectHandler) GetProjectUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, common.NewResponse(http.StatusOK, "프로젝트 참여자 목록 조회 완료", users))
 }
 
-// 프로젝트 수정
+// 프로젝트 정보 수정
 func (h *ProjectHandler) UpdateProject(c *gin.Context) {
-	// projectUsecase.UpdateProject(c)
-	// userId, exists := c.Get("userId")
-	// if !exists {
-	// 	fmt.Printf("인증되지 않은 사용자입니다.")
-	// 	c.JSON(http.StatusUnauthorized, common.NewError(http.StatusUnauthorized, "인증되지 않은 사용자입니다.", nil))
-	// 	return
-	// }
+	userId, exists := c.Get("userId")
+	if !exists {
+		fmt.Printf("인증되지 않은 사용자입니다.")
+		c.JSON(http.StatusUnauthorized, common.NewError(http.StatusUnauthorized, "인증되지 않은 사용자입니다.", nil))
+		return
+	}
 
-	// var request req.UpdateProjectRequest
-	// if err := c.ShouldBind(&request); err != nil {
-	// 	fmt.Printf("잘못된 요청입니다: %v", err)
-	// 	c.JSON(http.StatusBadRequest, common.NewError(http.StatusBadRequest, "잘못된 요청입니다", err))
-	// 	return
-	// }
+	projectID := c.Param("projectid")
+	parsedID, err := strconv.ParseUint(projectID, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, common.NewError(http.StatusBadRequest, "projectID 파싱 실패", err))
+		return
+	}
 
-	// err := h.projectUsecase.UpdateProject(userId.(uint), &request)
-	// if err != nil {
-	// 	if appError, ok := err.(*common.AppError); ok {
-	// 		c.JSON(appError.StatusCode, common.NewError(appError.StatusCode, appError.Message, appError.Err))
-	// 	} else {
-	// 		c.JSON(http.StatusInternalServerError, common.NewError(http.StatusInternalServerError, "서버 에러", err))
-	// 	}
-	// 	return
-	// }
+	var request req.UpdateProjectRequest
+	if err := c.ShouldBind(&request); err != nil {
+		fmt.Printf("잘못된 요청입니다: %v", err)
+		c.JSON(http.StatusBadRequest, common.NewError(http.StatusBadRequest, "잘못된 요청입니다", err))
+		return
+	}
 
-	// c.JSON(http.StatusOK, common.NewResponse(http.StatusOK, "프로젝트 수정 완료", nil))
+	request.ProjectID = uint(parsedID)
+	err = h.projectUsecase.UpdateProject(userId.(uint), &request)
+	if err != nil {
+		if appError, ok := err.(*common.AppError); ok {
+			c.JSON(appError.StatusCode, common.NewError(appError.StatusCode, appError.Message, appError.Err))
+		} else {
+			c.JSON(http.StatusInternalServerError, common.NewError(http.StatusInternalServerError, "서버 에러", err))
+		}
+		return
+	}
+	c.JSON(http.StatusOK, common.NewResponse(http.StatusOK, "프로젝트 수정 완료", nil))
 }
 
 // TODO 프로젝트 삭제
