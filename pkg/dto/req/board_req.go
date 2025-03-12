8b2e1da6149dfbfcea9b491f12c7e9fb9f0f4da8
이현종 <ehdclr@gractor.com>
2025-03-12 17:23:34 +0900

    change: 칸반보드 생성 로직 수정

--- pkg/dto/req/board_req.go
+++ pkg/dto/req/board_req.go
@@ -9,3 +9,35 @@ type UpdateBoardRequest struct {
 	Title     string `json:"title"`
 	ProjectID *uint  `json:"project_id"`
 }
+
+type BoardStateUpdateReqeust struct {
+	Changes []Change `json:"changes"`
+}
+
+type Change struct {
+	Type        string `json:"type" binding:"required"`
+	Action      string `json:"action" binding:"required"`
+	ProjectID   uint   `json:"project_id" binding:"required"`
+	BoardID     uint   `json:"board_id" binding:"required"`
+	ColumnID    uint   `json:"column_id,omitempty"`
+	CardID      uint   `json:"card_id,omitempty"`
+	Position    uint   `json:"position,omitempty"`
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+	StartDate   string `json:"start_date,omitempty"`
+	EndDate     string `json:"end_date,omitempty"`
+	Version     uint   `json:"version,omitempty"`
+}
+
+// 칸반보드 카드의 내용 변경
+type ChangeCardRequest struct {
+	CardID    uint   `json:"card_id" binding:"required"`
+	ProjectID uint   `json:"project_id" binding:"required"`
+	BoardID   uint   `json:"board_id" binding:"required"`
+	ColumnID  uint   `json:"column_id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Content   string `json:"content,omitempty"`
+	StartDate string `json:"start_date,omitempty"`
+	EndDate   string `json:"end_date,omitempty"`
+	Assignees []uint `json:"assignees,omitempty"`
+}
